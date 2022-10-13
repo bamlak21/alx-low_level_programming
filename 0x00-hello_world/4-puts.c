daa1bd118c9b90c529d105e6aa4029390e0cff3e
bamlak21 <bamlakgezahegn27@gmail.com>
2022-10-13 13:27:53 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "programming is like building a multilingual puzzle"
+ * main - prints a message
  * 
  * Return: 0 if exited properly, non-zero otherwise
  */
